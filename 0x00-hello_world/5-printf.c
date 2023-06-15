5db582cdcb0fbca48a3587c20c5879ee3473c85e
luckyalade <luckyalade309@gmail.com>
2023-06-15 11:51:21 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - print a string of code with printf
  *
- * Return: Always 0
+ * Return: 0
  */
 int main(void)
 {
-        printf(" with proper grammar, but the outcome is a piece of art,");
-        return (0);
+	printf(" with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
