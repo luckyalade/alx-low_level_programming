e9626f2ac3ea650e4400d2863cb426e7aa4ab4b4
luckyalade <luckyalade309@gmail.com>
2023-06-15 10:59:55 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,9 @@
  *
  * Return Always 0 (Success)
  */
-int main(void)
+   int main(void)
 {
-puts("\n Programming is like building a multilingual puzzle");
+   puts("\n\"Programming is like building a multilingual puzzle");
 	
-return (0);
+   return (0);
 }
